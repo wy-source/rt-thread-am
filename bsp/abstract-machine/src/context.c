21f01feaaa4e5e54293cbac295350fb9b02c3623
Wang, Yuan1 <yuan1.wang@intel.com>
2025-06-28 08:31:48 +0800

    [rt-thread] using globle variable for context switch
    Signed-off-by: Wang, Yuan1 <yuan1.wang@intel.com>

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -14,25 +14,32 @@ typedef struct {
   rt_ubase_t to_flag;
 } Ft;
 
+rt_ubase_t to_g;
+rt_ubase_t from_g;
+rt_ubase_t to_flag_g;
 
 static Context* ev_handler(Event e, Context *c) {
-  struct rt_thread *current = rt_thread_self();
-  Ft *ft = (Ft *)(current->user_data);
+  //struct rt_thread *current = rt_thread_self();
+  //Ft *ft = (Ft *)(current->user_data);
   switch (e.event) {
     case EVENT_YIELD: // Handle yield event
       printf("Got event yield %d\n", e.event);
       break;
     case EVENT_IRQ_TIMER: // Handle timer event
-      printf("Got event timer %d\n", e.event);
+      //printf("Got event timer %d\n", e.event);
       break;
     default: printf("Unhandled event ID = %d\n", e.event); assert(0);
   }
 
-  if (!ft->to_flag) {
-    *(Context **)(ft->from) = c;
-  }
-  c = *(Context **)(ft->to);
-  memset(ft, 0, sizeof(Ft)); // restore ft back to stack
+  if (!to_flag_g)
+    *(Context **)(from_g) = c;
+  to_flag_g = 0;
+  c = *(Context **)(to_g);
+  // if (!ft->to_flag) {
+  //   *(Context **)(ft->from) = c;
+  // }
+  // c = *(Context **)(ft->to);
+  // memset(ft, 0, sizeof(Ft)); // restore ft back to stack
 
   return c;
 }
@@ -42,28 +49,35 @@ void __am_cte_init() {
 }
 
 void rt_hw_context_switch_to(rt_ubase_t to) {
-  struct rt_thread *current = rt_thread_self();
-  rt_ubase_t user_data_tmp = current->user_data;
-  rt_ubase_t context_p = *(rt_ubase_t *)to;
-  Ft *ft = (Ft *) (context_p - sizeof(Ft));
-  ft->to = to;
-  ft->to_flag = 1;
-  current->user_data = (rt_ubase_t) ft;
+  to_g = to;
+  to_flag_g = 1;
   yield();
-  current->user_data = user_data_tmp;
+  // struct rt_thread *current = rt_thread_self();
+  // rt_ubase_t user_data_tmp = current->user_data;
+  // rt_ubase_t context_p = *(rt_ubase_t *)to;
+  // Ft *ft = (Ft *) (context_p - sizeof(Ft));
+  // ft->to = to;
+  // ft->to_flag = 1;
+  // current->user_data = (rt_ubase_t) ft;
+  // yield();
+  // current->user_data = user_data_tmp;
 }
 
 void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
-  struct rt_thread *current = rt_thread_self();
-  rt_ubase_t user_data_tmp = current->user_data;
-  rt_ubase_t context_p = *(rt_ubase_t *)to;
-  Ft *ft = (Ft *) (context_p - sizeof(Ft));
-  ft->to = to;
-  ft->from = from;
-  ft->to_flag = 0;
-  current->user_data = (rt_ubase_t) ft;
+  to_g = to;
+  from_g = from;
+  to_flag_g = 0;
   yield();
-  current->user_data = user_data_tmp;
+  // struct rt_thread *current = rt_thread_self();
+  // rt_ubase_t user_data_tmp = current->user_data;
+  // rt_ubase_t context_p = *(rt_ubase_t *)to;
+  // Ft *ft = (Ft *) (context_p - sizeof(Ft));
+  // ft->to = to;
+  // ft->from = from;
+  // ft->to_flag = 0;
+  // current->user_data = (rt_ubase_t) ft;
+  // yield();
+  // current->user_data = user_data_tmp;
 }
 
 void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t to, struct rt_thread *to_thread) {
