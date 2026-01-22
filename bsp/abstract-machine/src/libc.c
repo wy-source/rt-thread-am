3d6d5777b48b1ce4edb5b834dd5e94bbfccb28f1
seeker <gmx472@qq.com>
2026-01-22 14:45:26 +0800

    libc: mark string functions as weak

--- bsp/abstract-machine/src/libc.c
+++ bsp/abstract-machine/src/libc.c
@@ -1,22 +1,22 @@
 #include <klib.h>
 #include <rtthread.h>
 
-char *strchr(const char *s, int c) {
+__attribute__((weak)) char *strchr(const char *s, int c) {
   assert(0);
 }
 
-char *strrchr(const char *s, int c) {
+__attribute__((weak)) char *strrchr(const char *s, int c) {
   assert(0);
 }
 
-char *strstr(const char *haystack, const char *needle) {
+__attribute__((weak)) char *strstr(const char *haystack, const char *needle) {
   return rt_strstr(haystack, needle);
 }
 
-long strtol(const char *restrict nptr, char **restrict endptr, int base) {
+__attribute__((weak)) long strtol(const char *restrict nptr, char **restrict endptr, int base) {
   assert(0);
 }
 
-char *strncat(char *restrict dst, const char *restrict src, size_t sz) {
+__attribute__((weak)) char *strncat(char *restrict dst, const char *restrict src, size_t sz) {
   assert(0);
 }
