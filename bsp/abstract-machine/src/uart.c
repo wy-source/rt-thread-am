13ebe5f816579fdb8cb9b18f8cd57387452d8081
Wang, Yuan1 <hallo_sun@hotmail.com>
2026-03-10 04:02:48 +0000

    [uart] use getch to get input from the terminal
    Signed-off-by: Wang, Yuan1 <hallo_sun@hotmail.com>

--- bsp/abstract-machine/src/uart.c
+++ bsp/abstract-machine/src/uart.c
@@ -37,8 +37,9 @@ static int _uart_putc(struct rt_serial_device *serial, char c) {
 }
 
 static int _uart_getc(struct rt_serial_device *serial) {
-  static const char *p = "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
-  return (*p != '\0' ? *(p ++) : -1);
+  //static const char *p = "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
+  //return (*p != '\0' ? *(p ++) : -1);
+  return getch();
 }
 
 const struct rt_uart_ops _uart_ops = {
